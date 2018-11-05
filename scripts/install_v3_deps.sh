fb32589edfef784c5f47fbdd327c861eb2a197fe
Zander Mackie <amackie@pivotal.io>
2018-11-05 17:12:36 -0500

    WIP: /bin/supply.sh is not replaced. Builds v3 lifecycle binaries. Invokes them.
    Co-authored-by: Ben Fickes <bfickes@pivotal.io>

--- scripts/install_v3_deps.sh
+++ scripts/install_v3_deps.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
-if [[ ! -f .bin/v3lifecycle/detector ]]; then
+if [[ ! -f .bin/detector ]]; then
   (cd src/*/vendor/github.com/buildpack/lifecycle/cmd/detector && go install)
 fi
-if [[ ! -f .bin/v3lifecycle/builder ]]; then
+if [[ ! -f .bin/builder ]]; then
   (cd src/*/vendor/github.com/buildpack/lifecycle/cmd/builder && go install)
 fi
