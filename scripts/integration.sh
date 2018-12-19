358eb88873f1b959a2ec5e0d4bb8242c4c545c8b
Tyler Phelan <tphelan@pivotal.io>
2018-12-19 17:55:47 -0500

    WIP
    Co-authored-by: Guillermo Kardolus <gkardolus@pivotal.io>

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -6,20 +6,10 @@ source .envrc
 ./scripts/install_tools.sh
 
 GINKGO_NODES=${GINKGO_NODES:-3}
-GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-2}
+GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-1}
 export CF_STACK=${CF_STACK:-cflinuxfs3}
 
 pushd src/*/v3_integration
-
     echo "Run Uncached Shim Buildpack"
-    ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=60 -- --cached=false
-
+    ginkgo -r -v --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=60 -- --cached=false
 popd
-
-pushd src/*/integration
-    # echo "Run Uncached Buildpack"
-    # ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=60 -- --cached=false
-
-    # echo "Run Cached Buildpack"
-    # ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=60 -- --cached
- popd
